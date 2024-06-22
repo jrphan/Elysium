3c61b201004d1102dea29a0c884d21fb2423ecd5
Phan Duy Tam <phanduytam0306.ltp@gmail.com>
2024-06-22 11:33:50 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,32 @@
+let discover_collapse_01 = document.getElementById("discover_collapse_01");
+let discover_collapse_02 = document.getElementById("discover_collapse_02");
+let discover_collapse_03 = document.getElementById("discover_collapse_03");
+
+let collapse_title_01 = document.getElementById("collapse_title_01");
+let collapse_title_02 = document.getElementById("collapse_title_02");
+let collapse_title_03 = document.getElementById("collapse_title_03");
+
+let discover_btn_01 = document.getElementById("discover_btn_01");
+let discover_btn_02 = document.getElementById("discover_btn_02");
+let discover_btn_03 = document.getElementById("discover_btn_03");
+
+const toggleActive = (element, elementTitle) => {
+  element.classList.toggle("active");
+  elementTitle.classList.toggle("active");
+};
+
+discover_btn_01.onclick = function () {
+  toggleActive(discover_collapse_01, collapse_title_01);
+};
+
+discover_btn_02.onclick = function () {
+  toggleActive(discover_collapse_02, collapse_title_02);
+};
+
+discover_btn_03.onclick = function () {
+  toggleActive(discover_collapse_03, collapse_title_03);
+};
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
@@ -19,3 +48,6 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+var Scrollbar = window.Scrollbar;
+Scrollbar.init(document.querySelector("#wrapper_scrollbar"));
